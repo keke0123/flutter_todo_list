b803624443fd6716fc8819072a58a2621ba4e497
keke0123 <keke0123@naver.com>
2019-12-28 22:48:25 +0900

    login page 기본 완성

--- lib/layouts/loginPage.dart
+++ lib/layouts/loginPage.dart
@@ -9,34 +9,68 @@ class LoginPage extends StatelessWidget {
   };
   @override
   Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
     return Scaffold(
-      appBar: AppBar(title: Text('login page')),
+      appBar: AppBar(title: Text('Login Page')),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.center,
         // mainAxisSize: MainAxisSize.max,
         children: <Widget>[
-          Center(
+          // SizedBox(
+          //   height: 100,
+          // ),
+          Container(
+            margin: EdgeInsets.only(top: height / 4, bottom: 50),
+            width: width,
             child: Text(
-              'login page',
+              'Login Page',
               textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
             ),
           ),
-          Center(
-              child: TextField(
-            controller: formController['id'],
-          )),
-          FloatingActionButton(
-            onPressed: () {
-              return showDialog(
-                  context: context,
-                  builder: (context) {
-                    return AlertDialog(
-                        content: Text(formController['id'].text));
-                  });
-            },
-            child: Icon(Icons.text_fields),
+          Container(
+            width: width / 1.3,
+            child: TextField(
+              controller: formController['id'],
+              decoration: InputDecoration(
+                  // border: InputBorder.none,
+                  // focusedBorder: InputBorder.none,
+                  hintText: '아이디'),
+            ),
+          ),
+          Container(
+            width: width / 1.3,
+            child: TextField(
+              obscureText: true,
+              controller: formController['pwd'],
+              decoration: InputDecoration(
+                  // border: InputBorder.none,
+                  // focusedBorder: InputBorder.none,
+                  hintText: '패스워드'),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 15),
+            width: width / 1.3,
+            height: 40,
+            child: RaisedButton(
+              child: Text('Submit'),
+              onPressed: () => {print(formController)},
+            ),
           )
+          // FloatingActionButton(
+          //   onPressed: () {
+          //     return showDialog(
+          //         context: context,
+          //         builder: (context) {
+          //           return AlertDialog(
+          //               content: Text(formController['id'].text));
+          //         });
+          //   },
+          //   child: Icon(Icons.text_fields),
+          // )
         ],
       ),
     );
