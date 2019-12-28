6de430963e4727fc76acf83914d26b54ab55e395
keke0123 <keke0123@naver.com>
2019-12-28 21:29:11 +0900

    input 에 글자 입력후 플로팅 버튼 누르면 dialog 생성후 글자 출력

--- lib/layouts/loginPage.dart
+++ lib/layouts/loginPage.dart
@@ -1,12 +1,43 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
+  final formController = {
+    'id': TextEditingController(),
+    'pwd': TextEditingController()
+  };
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('login page')),
-      body: Center(
-        child: Text('login'),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        // mainAxisSize: MainAxisSize.max,
+        children: <Widget>[
+          Center(
+            child: Text(
+              'login page',
+              textAlign: TextAlign.center,
+            ),
+          ),
+          Center(
+              child: TextField(
+            controller: formController['id'],
+          )),
+          FloatingActionButton(
+            onPressed: () {
+              return showDialog(
+                  context: context,
+                  builder: (context) {
+                    return AlertDialog(
+                        content: Text(formController['id'].text));
+                  });
+            },
+            child: Icon(Icons.text_fields),
+          )
+        ],
       ),
     );
   }
