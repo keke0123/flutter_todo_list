3aa94fb19ce0d7b466420b51122a1854c4a573a5
keke0123 <keke0123@naver.com>
2019-12-28 19:53:01 +0900

    login 하지 않은 상태에서 main 페이지로 접근시 튕기게 하는 처리 작성

--- lib/main.dart
+++ lib/main.dart
@@ -7,24 +7,6 @@ import 'package:provider/provider.dart';
 
 void main() => runApp(App());
 
-// class App extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'app',
-//       home: MultiProvider(
-//         providers: [
-//           ChangeNotifierProvider(
-//             builder: (_) => UserProvider(),
-//           )
-//         ],
-//         child: MainPage(),
-//       ),
-//       routes: routes,
-//     );
-//   }
-// }
-
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -34,25 +16,41 @@ class App extends StatelessWidget {
           builder: (_) => UserProvider(),
         )
       ],
-      child: MaterialApp(
-        title: 'app',
-        // routes: routes,
-        initialRoute: '/main',
-        onGenerateRoute: _getRoute,
-      ),
+      child: AppSub(),
+    );
+  }
+}
+
+class AppSub extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    UserProvider _data = Provider.of<UserProvider>(context);
+    print('App Sub');
+    // _data.user = {'id': 'test user', 'pwd': '123123'};
+    return MaterialApp(
+      title: 'app',
+      // routes: routes,
+      initialRoute: '/main',
+      onGenerateRoute: (settings) => _getRoute(settings, _data.user),
     );
   }
 
   //
-  Route _getRoute(RouteSettings settings) {
+  Route _getRoute(RouteSettings settings, dynamic type) {
+    print('generate router');
+    print(type);
     switch (settings.name) {
+      // switch (type) {
       case '/main':
         print('get route main page');
-        print(settings);
+        if (type == null) {
+          return MaterialPageRoute(builder: (_) => LoginPage());
+        }
+        // print(settings);
         return MaterialPageRoute(builder: (_) => MainPage());
       case '/login':
         print('get route login page');
-        print(settings);
+        // print(settings);
         return MaterialPageRoute(builder: (_) => LoginPage());
     }
   }
