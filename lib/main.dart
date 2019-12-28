c44fdb17c23d05210a3cad96ece4fc5daeb763ce
keke0123 <keke0123@naver.com>
2019-12-28 20:08:57 +0900

    router 부분 따로 class 로 잡아서 뺌

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_todo_list/layouts/loginPage.dart';
-import 'package:flutter_todo_list/layouts/mainPage.dart';
 import 'package:flutter_todo_list/providers/userProvider.dart';
 import 'package:flutter_todo_list/routes.dart';
 import 'package:provider/provider.dart';
@@ -28,30 +26,9 @@ class AppSub extends StatelessWidget {
     print('App Sub');
     // _data.user = {'id': 'test user', 'pwd': '123123'};
     return MaterialApp(
-      title: 'app',
-      // routes: routes,
-      initialRoute: '/main',
-      onGenerateRoute: (settings) => _getRoute(settings, _data.user),
-    );
-  }
-
-  //
-  Route _getRoute(RouteSettings settings, dynamic type) {
-    print('generate router');
-    print(type);
-    switch (settings.name) {
-      // switch (type) {
-      case '/main':
-        print('get route main page');
-        if (type == null) {
-          return MaterialPageRoute(builder: (_) => LoginPage());
-        }
-        // print(settings);
-        return MaterialPageRoute(builder: (_) => MainPage());
-      case '/login':
-        print('get route login page');
-        // print(settings);
-        return MaterialPageRoute(builder: (_) => LoginPage());
-    }
+        title: 'app',
+        initialRoute: '/main',
+        onGenerateRoute: (settings) =>
+            new AppRouter().getRoute(settings, _data.user));
   }
 }
--- lib/routes.dart
+++ lib/routes.dart
@@ -1,11 +1,25 @@
-library Router;
-
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_todo_list/layouts/loginPage.dart';
 import 'package:flutter_todo_list/layouts/mainPage.dart';
 
-final routes = {
-  '/login': (BuildContext context) => LoginPage(),
-  '/main': (BuildContext context) => MainPage(),
-  //
-};
+class AppRouter {
+  Route getRoute(RouteSettings settings, dynamic type) {
+    print('generate router');
+    print(type);
+    switch (settings.name) {
+      // switch (type) {
+      case '/main':
+        print('get route main page');
+        if (type == null) {
+          return MaterialPageRoute(builder: (_) => LoginPage());
+        }
+        // print(settings);
+        return MaterialPageRoute(builder: (_) => MainPage());
+      case '/login':
+        print('get route login page');
+        // print(settings);
+        return MaterialPageRoute(builder: (_) => LoginPage());
+    }
+  }
+}
