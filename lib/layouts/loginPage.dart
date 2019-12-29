4704d0b94e011d5eb46c5ef321114046a9a152bf
keke0123 <keke0123@naver.com>
2019-12-29 12:39:20 +0900

    submit 버튼 validation 체크후 snack bar 로 표시해주는것 제작 / context 가 안먹어서 global key 로 처리하는 방식 또는 builder 로 전달하는 방식 두개가 있는데 builder 로 전달하는 방식으로 해결

--- lib/layouts/loginPage.dart
+++ lib/layouts/loginPage.dart
@@ -1,16 +1,20 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_todo_list/providers/userProvider.dart';
+import 'package:provider/provider.dart';
 
 class LoginPage extends StatelessWidget {
   final formController = {
     'id': TextEditingController(),
     'pwd': TextEditingController()
   };
+  // final formKey = {'id': GlobalKey<FormState>(), 'pwd': GlobalKey<FormState>()};
   @override
   Widget build(BuildContext context) {
     double width = MediaQuery.of(context).size.width;
     double height = MediaQuery.of(context).size.height;
+    UserProvider _data = Provider.of<UserProvider>(context);
     return Scaffold(
       appBar: AppBar(title: Text('Login Page')),
       body: Column(
@@ -32,34 +36,64 @@ class LoginPage extends StatelessWidget {
           ),
           Container(
             width: width / 1.3,
-            child: TextField(
+            child: TextFormField(
               controller: formController['id'],
               decoration: InputDecoration(
-                  // border: InputBorder.none,
-                  // focusedBorder: InputBorder.none,
-                  hintText: '아이디'),
+                labelText: 'id',
+                // border: InputBorder.none,
+                // focusedBorder: InputBorder.none,
+                // hintText: '아이디'
+              ),
             ),
           ),
           Container(
             width: width / 1.3,
-            child: TextField(
+            child: TextFormField(
+              // key: formKey['pwd'],
               obscureText: true,
               controller: formController['pwd'],
               decoration: InputDecoration(
-                  // border: InputBorder.none,
-                  // focusedBorder: InputBorder.none,
-                  hintText: '패스워드'),
+                labelText: 'pwd',
+                // border: InputBorder.none,
+                // focusedBorder: InputBorder.none,
+                // hintText: '패스워드'
+              ),
+              validator: (value) {
+                print('validation');
+                return 'pwd good';
+              },
             ),
           ),
           Container(
-            margin: EdgeInsets.only(top: 15),
-            width: width / 1.3,
-            height: 40,
-            child: RaisedButton(
-              child: Text('Submit'),
-              onPressed: () => {print(formController)},
-            ),
-          )
+              margin: EdgeInsets.only(top: 15),
+              width: width / 1.3,
+              height: 40,
+              child: new Builder(
+                builder: (context) {
+                  return RaisedButton(
+                    child: Text('Submit'),
+                    onPressed: () {
+                      print('submit');
+                      print(context);
+                      print(formController['id'].text);
+                      print(formController['pwd'].text);
+                      print(formController['pwd']);
+                      if (formController['id'].text.isEmpty ||
+                          formController['pwd'].text.isEmpty) {
+                        Scaffold.of(context).showSnackBar(SnackBar(
+                          content: Text('ID or PWD 를 입력해 주세요'),
+                        ));
+                        return null;
+                      }
+                      _data.user = {
+                        'id': formController['id'].text,
+                        'pwd': formController['pwd'].text
+                      };
+                      Navigator.of(context).pushReplacementNamed('/main');
+                    },
+                  );
+                },
+              ))
           // FloatingActionButton(
           //   onPressed: () {
           //     return showDialog(
